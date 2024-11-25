61133f440ea87270a8e4f52c8b9237bd66dfde25
Anurag Kumar <138526681+anurag211824@users.noreply.github.com>
2024-11-26 02:21:52 +0530

    learn nodejs module

---
+++ NodeJsModules/app.js
@@ -0,0 +1,4 @@
+// const {add,multiply}=require("./math.js")
+const math = require("./math");
+console.log(math.add(12, 7));
+console.log(math.multiply(12, 3));
---
+++ NodeJsModules/math.js
@@ -0,0 +1,15 @@
+const add = (a, b) => {
+  return a + b;
+};
+const multiply = (a, b) => {
+  return a * b;
+};
+
+// module.exports=add
+// module.exports.add=add
+// module.exports.multiply=multiply
+
+module.exports = {
+  add,
+  multiply,
+};

f096a92edb7d81ad37f21141947c1d1ca178703e
Anurag Kumar <138526681+anurag211824@users.noreply.github.com>
2024-11-28 17:56:52 +0530

    leran path,os and crypto module in nodeJs

---
+++ 4CryptoModule/crypto.js
@@ -0,0 +1,12 @@
+const crypto=require("crypto")
+
+const randomValue=crypto.randomBytes(8).toString("hex");
+
+console.log(randomValue);
+
+const hashValue1=crypto.createHash("sha256").update("anuragKumar").digest("hex")
+console.log(hashValue1);
+
+const hashValue2=crypto.createHash("sha256").update("abhinav").digest("hex")
+console.log(hashValue2);
+
